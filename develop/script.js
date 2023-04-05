33e0a1f8dc753d56a756eecc305b263dbed3cc85
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-04-05 19:17:46 -0400

    Add files via upload

---
+++ develop/script.js
@@ -0,0 +1,39 @@
+// define character sets for password generation
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+// prompt the user for password criteria
+var length = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
+var includeLowercase = confirm("Include lowercase characters?");
+var includeUppercase = confirm("Include uppercase characters?");
+var includeNumeric = confirm("Include numeric characters?");
+var includeSpecial = confirm("Include special characters?");
+
+// generate password based on criteria
+var characters = "";
+if (includeLowercase) {
+  characters += lowercase;
+}
+if (includeUppercase) {
+  characters += uppercase;
+}
+if (includeNumeric) {
+  characters += numeric;
+}
+if (includeSpecial) {
+  characters += special;
+}
+
+var password = "";
+for (var i = 0; i < length; i++) {
+  var randomIndex = Math.floor(Math.random() * characters.length);
+  password += characters[randomIndex];
+}
+
+// output password to the console
+console.log("Your new password is: " + password);
+
+// display the password to the user
+alert("Your password is: " + password);

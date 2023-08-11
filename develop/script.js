53cc57d0deab90d0f49c019cccbcb7c8b100180d
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-08-11 13:10:07 -0400

    Update script.js

--- develop/script.js
+++ develop/script.js
@@ -1,36 +1,57 @@
-// define character sets for password generation
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numeric = "0123456789";
-var special = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// prompt the user for password criteria
-var length = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
-var includeLowercase = confirm("Include lowercase characters?");
-var includeUppercase = confirm("Include uppercase characters?");
-var includeNumeric = confirm("Include numeric characters?");
-var includeSpecial = confirm("Include special characters?");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// generate password based on criteria
-var characters = "";
-if (includeLowercase) {
-  characters += lowercase;
-}
-if (includeUppercase) {
-  characters += uppercase;
-}
-if (includeNumeric) {
-  characters += numeric;
-}
-if (includeSpecial) {
-  characters += special;
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
-var password = "";
-for (var i = 0; i < length; i++) {
-  var randomIndex = Math.floor(Math.random() * characters.length);
-  password += characters[randomIndex];
-}
+// Function to generate password based on user input
+function generatePassword() {
+  // Define character sets
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+[]{}|;:',.<>?";
 
-// display the password to the user
-alert("Your password is: " + password);
+  // Initialize variables for password criteria
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+  var passwordLength = prompt("Choose password length (8-128 characters):");
+
+  // Validate password length
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return "";
+  }
+
+  // Validate at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least one character type.");
+    return "";
+  }
+
+  // Build the character set based on selected criteria
+  var charSet = "";
+  if (includeLowercase) charSet += lowercaseChars;
+  if (includeUppercase) charSet += uppercaseChars;
+  if (includeNumeric) charSet += numericChars;
+  if (includeSpecial) charSet += specialChars;
+
+  // Generate the password
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+
+  return password;
+}
